9012a4420f4119cc9733679ee4853a71f355a511
Avi Shah <115107522+avishah3@users.noreply.github.com>
2025-02-06 10:58:06 -0500

    Added comments

--- main.py
+++ main.py
@@ -6,11 +6,13 @@ if __name__ == "__main__":
     # 0. There are a lot of requirements to create/use OpenCV projects. I used the following video to get started
     # https://youtu.be/WgPbbWmnXJ8
 
+    # If you want to train your own model, follow the steps below. Otherwise, use the pre-trained best.pt model
+
     # 1. Download the dataset specified in config.yaml
     # 2. Put the downloaded folders into your project and change absolute paths in config.yaml
     # 3. Change the following text to the correct relative paths for your project
     # 4. Run this
-    # 5. Go to runs/detect/train/weights and use best.pt as the model while running shot_detector.py
+    # 5. Go to runs/detect/train/weights and use that best.pt as the model while running shot_detector.py
 
     # Select device for training
     device = get_device()
